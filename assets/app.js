7e39726dfe9dd4e7b88712064f7a1546894aecf0
Emanuel Bitkov <emanuel.bitkov@regenta-verlag.de>
2025-10-19 14:30:05 +0200

    Update App
    - Upgrade to PHP 8.4
    - Upgrade to Symfony 7.3
    - Update bundle recipes:
      - symfony/ux-twig-component
      - symfony/ux-live-component
      - symfony/translation
      - symfony/stimulus-bundle
      - symfony/routing
      - doctrine/doctrine-bundle

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,9 @@
+// Stimulus
+import './bootstrap.js';
+
 // Bootstrap CSS Framework
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Stimulus Bootstrap
-import './bootstrap.js';
 
 // App Styles
 import './fonts/DM_Serif_Text.css';
